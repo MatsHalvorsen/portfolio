1b17e344a9be4a4fc046ad2c660c0669eb74c321
M Stark <mats.halvorsen@outlook.com>
2023-04-21 15:36:33 +0200

    added pictures to every project

--- js/script.js
+++ js/script.js
@@ -7,3 +7,20 @@ $(document).ready(function() {
     });
   });
 
+  const scrollToTopBtn = document.getElementById("scroll-to-top");
+
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  });
+  
+  scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
+  
